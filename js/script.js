7d0d3d181be7203a6f98e80167b9029dd06b2bc1
Zemtsov Igor <zemtsovigor@yahoo.com>
2024-09-25 23:37:25 +0200

    added menu

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
   // add scroll on menu item click
   $('a[href^="#"]').on('click', function(event) {
+    var bodyHasClass = $('body').hasClass('-mobile_menu_opened');
     var target = $(this.getAttribute('href'));
     if(target.length) {
       event.preventDefault();
@@ -9,8 +10,21 @@ $(document).ready(function() {
         scrollTop: target.offset().top
       }, 1000);
     }
+
+    if (bodyHasClass) {
+      $('.navbar-toggler').click();
+    }
   });
 
   $('.simple-bar').each((index, element) => new SimpleBar(element));
+
+  $('.navbar-toggler').on( "click", function() {
+    var bodyHasClass = $('body').hasClass('-mobile_menu_opened');
+    if (bodyHasClass) {
+      $('body').removeClass('-mobile_menu_opened');
+    } else {
+      $('body').addClass('-mobile_menu_opened');
+    }
+  });
 
 });
